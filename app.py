23785e888005266523681655a3d1a749c66f7d45
root <root@Acer-Alex>
2025-06-25 16:32:14 +0200

    clean code

--- app.py
+++ app.py
@@ -6,11 +6,11 @@ import os
 load_dotenv()
 
 from config import Config
-from extensions import db  # ✅ import propre depuis extensions.py
+from extensions import db
 
 app = Flask(__name__)
 app.config.from_object(Config)
-db.init_app(app)  # ✅ on initialise ici
+db.init_app(app)
 
 def test_db_connection():
     try:
@@ -19,13 +19,15 @@ def test_db_connection():
     except Exception as e:
         print("Erreur de connexion à la BDD :", e)
 
+from models.company import Company
+
 @app.route('/')
 def home():
     return jsonify({"message": "Bienvenue !"})
 
 @app.route('/companies', methods=['GET'])
 def get_companies():
-    from models.company import Company  # import local pour éviter des surprises
+    from models.company import Company
     companies = Company.query.all()
     return jsonify([c.to_dict() for c in companies])
 
